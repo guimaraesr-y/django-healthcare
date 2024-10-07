5ab961683f52106a4d7ab33e5261f00291dcff66
Ryan Guimarães <guimaraesryan26@gmail.com>
2024-10-07 10:47:45 -0300

    refactor: change explicit imports to wildcard import

--- firstproject/api/urls.py
+++ firstproject/api/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import PatientViewSet
-from .views import CollaboratorViewSet
+from .views import *
 
 router = DefaultRouter()
 router.register(r"collaborators", CollaboratorViewSet)
