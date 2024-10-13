4a9db9e39c07c838ee88366058e9514eb45a8af6
Ryan Guimarães <guimaraesryan26@gmail.com>
2024-10-13 01:08:33 -0300

    fix: fix tests

--- firstproject/api/tests/test_collaborator.py
+++ firstproject/api/tests/test_collaborator.py
@@ -1,11 +1,19 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
 
+from ..models import Collaborator
+
 
 class TestCollaboratorCrud(TestCase):
     def setUp(self):
         self.client = APIClient()
         self.client.force_authenticate(user=None)
+
+        self.collaborator = Collaborator.objects.create(
+            name="John Doe",
+            email="john@example.com",
+            phone="123-456-7890",
+        )
 
     def test_create_collaborator(self):
         data = {
@@ -15,10 +23,24 @@ class TestCollaboratorCrud(TestCase):
         }
         response = self.client.post("/api/collaborators/", data=data)
         self.assertEqual(response.status_code, 201)
+    
+    def test_create_collaborator_with_invalid_data(self):
+        data = {
+            "name": "Jane Doe",
+            "email": "jane",
+        }
+        
+        response = self.client.post("/api/collaborators/", data=data)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {
+            "email": ["Enter a valid email address."],
+            "phone": ["This field is required."]
+        })
 
     def test_retrieve_collaborator(self):
         response = self.client.get("/api/collaborators/")
         self.assertEqual(response.status_code, 200)
+        self.assertGreater(len(response.json()), 0)
 
     def test_update_collaborator(self):
         data = {
@@ -26,9 +48,11 @@ class TestCollaboratorCrud(TestCase):
             "email": "john@example.com",
             "phone": "123-456-7890",
         }
-        response = self.client.put("/api/collaborators/1/", data=data)
+        response = self.client.put(
+            f"/api/collaborators/{self.collaborator.pk}/", data=data
+        )
         self.assertEqual(response.status_code, 200)
 
     def test_delete_collaborator(self):
-        response = self.client.delete("/api/collaborators/1/")
+        response = self.client.delete(f"/api/collaborators/{self.collaborator.pk}/")
         self.assertEqual(response.status_code, 204)
--- firstproject/api/tests/test_patient.py
+++ firstproject/api/tests/test_patient.py
@@ -1,11 +1,21 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
 
+from ..models import Patient
+
 
 class TestPatientCrud(TestCase):
     def setUp(self):
         self.client = APIClient()
         self.client.force_authenticate(user=None)
+        
+        self.patient = Patient.objects.create(
+            name="John Doe",
+            age=30,
+            gender="Male",
+            email="john@example.com",
+            phone="123-456-7890",
+        )
 
     def test_create_patient(self):
         data = {
@@ -20,19 +30,23 @@ class TestPatientCrud(TestCase):
 
     def test_retrieve_patient(self):
         response = self.client.get("/api/patients/")
+        json = response.json()
+        
         self.assertEqual(response.status_code, 200)
+        self.assertGreater(len(json), 0)
 
     def test_update_patient(self):
         data = {
-            "name": "John Doe",
-            "age": 30,
+            "name": "John Doe edited",
+            "age": 35,
             "gender": "Male",
             "email": "john@example.com",
             "phone": "123-456-7890",
         }
         response = self.client.put("/api/patients/1/", data=data)
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json().get("name"), "John Doe edited")
 
     def test_delete_patient(self):
-        response = self.client.delete("/api/patients/1/")
+        response = self.client.delete(f"/api/patients/{self.patient.pk}/")
         self.assertEqual(response.status_code, 204)
